b962a378e85da904c59a1ee2b429b236e7362e53
hareesh43 <hareeshgummani@gmail.com>
2021-03-16 16:38:03 +0530

    initial setup

---
+++ src/pages/Create.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+export default function Create() {
+  return (
+    <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
+      Create Note
+    </Typography>
+  );
+}
