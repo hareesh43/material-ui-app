e368d9ef91ae78334b5e4325f977dac2edf1b3b0
hareesh43 <hareeshgummani@gmail.com>
2021-03-16 17:19:55 +0530

    added submit icon

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Container from '@material-ui/core/Container';
+import SendIcon from '@material-ui/icons/Send';
 
 export default function Create() {
   return (
-    <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
-      Create Note
-    </Typography>
+    <Container>
+      <Typography
+        variant="h6"
+        component="h2"
+        color="textSecondary"
+        gutterBottom
+      >
+        Create Note
+      </Typography>
+      <Button variant="contained" color="primary" type="submit"
+      endIcon = {<SendIcon/>}
+      >
+        Submit
+      </Button>
+    </Container>
   );
 }
