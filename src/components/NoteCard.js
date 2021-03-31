11097073982c84dfab73fd1b7f9b19a928e3f621
hareesh43 <hareeshgummani@gmail.com>
2021-03-31 11:01:01 +0530

    added avatars to navbar and cards

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -2,16 +2,27 @@ import React from "react";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import Card from "@material-ui/core/Card";
-import { IconButton, makeStyles, Typography } from "@material-ui/core";
+import { Avatar, IconButton, makeStyles, Typography } from "@material-ui/core";
 import { DeleteOutlined } from "@material-ui/icons";
+import { blue, green, red, yellow } from "@material-ui/core/colors";
 
 const useStyles = makeStyles({
-  test: {
-    border: (notes) => {
-      if (notes.category === "work") {
-        return "1px solid red";
-      }
-    },
+  avatar: {
+   backgroundColor:(notes)=>{
+     if(notes.category === 'work'){
+       return yellow[700]
+     }
+     if(notes.category === 'reminders'){
+      return green[500]
+    }
+    if(notes.category === 'todos'){
+      return red[400]
+    }
+    if(notes.category === 'money'){
+      return blue[700]
+    }
+   
+   }
   },
 });
 
@@ -19,8 +30,14 @@ export default function NoteCard({ notes, handleDelete }) {
   const classes = useStyles(notes);
   return (
     <div>
-      <Card elevation={2} className={classes.test}>
+      <Card elevation={2} className={classes.test}
+      >
+    
         <CardHeader
+        avatar = {<Avatar className={classes.avatar}>
+          {notes.category[0].toUpperCase()}
+          
+        </Avatar>}
           action={
             <IconButton onClick={() => handleDelete(notes.id)}>
               <DeleteOutlined />
